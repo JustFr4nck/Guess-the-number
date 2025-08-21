108fa583604934e9a8e6e441e2a00f110ca2c032
JustFr4nck <francyperry07@gmail.com>
2025-08-21 23:36:25 +0200

    menu release

---
+++ script.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string name;
+
+    cout << "Inserisci il tuo nome" << endl;
+    cin >> name;
+
+    cout << "Bene " << name << ",\nDigita 1 per la difficoltà facile;\nDigita 2 per la difficoltà media;\nDigita 3 per la difficoltà difficile;\nDigita 0 per chiudere il programma" << endl;
+
+}
